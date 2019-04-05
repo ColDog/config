7a09c9a6696d3017790140c47773c997957231d9
Colin Walker <colinwalker270@gmail.com>
2019-04-04 20:48:02 -0700

    Fix install vim plug.

--- init.sh
+++ init.sh
@@ -39,6 +39,7 @@ echo ">>> sync dotfiles"
 ./dotfiles/load.sh
 
 echo ">>> install vim"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +'PlugInstall --sync' +qa
 
 echo ">>> add to bashrc"
